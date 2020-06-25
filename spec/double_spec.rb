a4ced50fd55a47fe52d6d19bdd28dcf686f390d2
Liam Tinney <ltinney@powerhrg.com>
2020-06-25 15:44:27 -0400

    Done.

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,36 +1,30 @@
 require "spec_helper"
 
 RSpec.describe "Doubles" do
-  # Instead of using instances in these tests, 
+  # Instead of using instances in these tests,
   # replace each of these with a 'leaf' test double
   it "leaf returns expected color" do
-    tree = Tree.new
-    branch = Branch.new(tree)
-    leaf = Leaf.new(branch)
+    leaf = double("leaf", :color => "green")
 
     expect(leaf.color).to eq("green")
-
     expect(leaf).to_not be_instance_of(Leaf)
   end
 
   it "leaf's branch returns expected length" do
-    tree = Tree.new
-    branch = Branch.new(tree)
-    leaf = Leaf.new(branch)
+    branch = double("branch", :length => "4 feet")
+    leaf = double("leaf", :branch => branch)
 
     expect(leaf.branch.length).to eq("4 feet")
-
     expect(leaf).to_not be_instance_of(Leaf)
     expect(leaf.branch).to_not be_instance_of(Branch)
   end
 
   it "leaf's branch's tree returns expected bark" do
-    tree = Tree.new
-    branch = Branch.new(tree)
-    leaf = Leaf.new(branch)
+    tree = double("tree", :bark => "thick and brown")
+    branch = double("branch", :tree => tree)
+    leaf = double("leaf", :branch => branch)
 
     expect(leaf.branch.tree.bark).to eq("thick and brown")
-
     expect(leaf).to_not be_instance_of(Leaf)
     expect(leaf.branch).to_not be_instance_of(Branch)
     expect(leaf.branch.tree).to_not be_instance_of(Tree)
